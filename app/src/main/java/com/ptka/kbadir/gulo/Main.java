de06d79782ee6ed69f171f607a2835253bd613e8
Faisal Rifai <frifai11.fr@gmail.com>
2017-11-20 16:56:55 +0700

    update jadi

--- app/src/main/java/com/ptka/kbadir/gulo/Main.java
+++ app/src/main/java/com/ptka/kbadir/gulo/Main.java
@@ -1,24 +1,41 @@
 package com.ptka.kbadir.gulo;
 
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
+import it.neokree.materialtabs.MaterialTab;
 import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
 
-public class Main extends AppCompatActivity {
+public class Main extends ActionBarActivity implements MaterialTabListener{
+
+    private ViewPager pager;
+    private ViewPagerAdapter pagerAdapter;
     MaterialTabHost tabHost;
-    ViewPager pager;
-
-
+    private Resources res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        res = this.getResources();
+        // init toolbar (old action bar)
+
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        this.setSupportActionBar(toolbar);
 
         tabHost = (MaterialTabHost) this.findViewById(R.id.tabHost);
         pager = (ViewPager) this.findViewById(R.id.pager);
-
         // init view pager
         pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         pager.setAdapter(pagerAdapter);
@@ -29,7 +46,6 @@ public class Main extends AppCompatActivity {
                 tabHost.setSelectedNavigationItem(position);
             }
         });
-
         // insert all tabs from pagerAdapter data
         for (int i = 0; i < pagerAdapter.getCount(); i++) {
             tabHost.addTab(
@@ -39,11 +55,54 @@ public class Main extends AppCompatActivity {
             );
         }
     }
-
     @Override
     public void onTabSelected(MaterialTab tab) {
-        // when the tab is clicked the pager swipe content to the tab position
+// when the tab is clicked the pager swipe content to the tab position
         pager.setCurrentItem(tab.getPosition());
-
     }
+
+    @Override
+    public void onTabReselected(MaterialTab tab) {
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab tab) {
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        public Fragment getItem(int num) {
+            return new FragmentText();
+        }
+        @Override
+        public int getCount() {
+            return 3;
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position) {
+                case 0: return "Product";
+                case 1: return "Sewa";
+                case 2: return "Riwayat";
+                default: return null;
+            }
+        }
+    }
+    /*
+    * It doesn't matter the color of the icons, but they must have solid colors
+    */
+    private Drawable getIcon(int position) {
+        switch(position) {
+            case 0:
+                return res.getDrawable(R.drawable.left_arrow);
+            case 1:
+                return res.getDrawable(R.drawable.ic_launcher_background);
+            case 2:
+                return res.getDrawable(R.drawable.right_arrow);
+        }
+        return null;
+    }
+
 }
